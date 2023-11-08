10850e231abbd48d4836187eabde3220707c3b59
iroda743 <127469283+iroda743@users.noreply.github.com>
2023-11-08 21:40:57 +0500

    Update task_1.h

--- src/task_1.h
+++ src/task_1.h
@@ -1,5 +1,26 @@
-/*
- * Author:
- * Date:
- * Name:
- */
+class Node {
+public:
+    int data;
+    Node* next;
+
+    //Constructor
+    Node(int val){
+        this->data = val;
+        this->next = NULL;
+    }
+};
+
+class Stack{
+    Node* head;
+public:
+    //Constructor
+    Stack(){
+        this->head = NULL;
+    }
+    void push(int val){
+        Node* tmp = new Node(val);
+        tmp->data = val;
+        tmp->next = this->head;
+        this->head = tmp;
+    }
+};
